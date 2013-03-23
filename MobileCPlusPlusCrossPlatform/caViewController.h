25e4c264d6c5a92eb624272e0422932f259e18e3
Shawn Campbell <jaegerpicker@gmail.com>
2013-03-22 23:24:06 -0400

    Initnal setup and commit

---
+++ MobileCPlusPlusCrossPlatform/caViewController.h
@@ -0,0 +1,13 @@
+//
+//  caViewController.h
+//  MobileCPlusPlusCrossPlatform
+//
+//  Created by Shawn Campbell on 03/22/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface caViewController : UIViewController
+
+@end
---
+++ MobileCPlusPlusCrossPlatformTests/MobileCPlusPlusCrossPlatformTests.h
@@ -0,0 +1,13 @@
+//
+//  MobileCPlusPlusCrossPlatformTests.h
+//  MobileCPlusPlusCrossPlatformTests
+//
+//  Created by Shawn Campbell on 03/22/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MobileCPlusPlusCrossPlatformTests : SenTestCase
+
+@end
