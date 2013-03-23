ebd1769a6dd1c44a35309ceafbd206dde6fa4901
jaegerpicker@gmail.com <jaegerpicker@gmail.com>
2013-03-23 01:16:22 -0400

    First win phone 8 commit

---
+++ MobileCPlusPlusWinPhone8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MobileCPlusPlusWinPhone8.Resources;
+
+namespace MobileCPlusPlusWinPhone8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
