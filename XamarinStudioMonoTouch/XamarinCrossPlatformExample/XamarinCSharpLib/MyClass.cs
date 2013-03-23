3407eaefb13263dd77e1aa46e98eef48ff232fce
Shawn Campbell <jaegerpicker@gmail.com>
2013-03-23 01:31:23 -0400

    Xamarin updates

---
+++ XamarinStudioMonoTouch/XamarinCrossPlatformExample/XamarinCSharpLib/MyClass.cs
@@ -0,0 +1,15 @@
+using System;
+using MonoGame.Framework;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics.ES20;
+
+namespace XamarinCSharpLib
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
