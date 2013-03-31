a06bd38babe590cb46ae77cb22a7bf1e56164ab2
Shawn Campbell <jaegerpicker@gmail.com>
2013-03-31 02:57:11 -0400

    Got iOS sample most of the way there

--- MobileCPlusPlusCrossPlatfromPyWebSocketServer/MobileCPlusPlusCrossPlatformWebSocketServer/chatServer.py
+++ MobileCPlusPlusCrossPlatfromPyWebSocketServer/MobileCPlusPlusCrossPlatformWebSocketServer/chatServer.py
@@ -1,55 +1,86 @@
-#
-
-
-import tornado.httpserver
-import tornado.websocket
-import tornado.ioloop
-import tornado.web
+import logging
 import tornado.escape
-import sockjs.tornado
-import sys
-import os
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import os.path
+import uuid
 
-class chatServer(sockjs.tornado.SockJSConnection):
-    currSession = 0
-    clients = set()
+from tornado.options import define, options
 
-    def on_open(self, info):
-        print "WebSocket open"
-        self.clients.add(self)
-        self.broadcast(self.clients, "Someone Joined")
-
-    def on_message(self, message):
-        print "message received"
-        #inputs = tornado.escape.json_decode(message)
-        self.broadcast(self.clients, message)
+define("port", default=8765, help="run on the given port", type=int)
 
 
-    def on_close(self):
-        print "WebSocket Close"
-        self.clients.remove(self)
-
-        self.broadcast(self.clients, "Someone left.")
-
-
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/chatsocket", ChatSocketHandler),
+            ]
+        settings = dict(
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+            )
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        return "no root config"
+        self.render("index.html", messages=ChatSocketHandler.cache)
+
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    waiters = set()
+    cache = []
+    cache_size = 200
+
+    def allow_draft76(self):
+        # for iOS 5.0 Safari
+        return True
+
+    def open(self):
+        ChatSocketHandler.waiters.add(self)
+
+    def on_close(self):
+        ChatSocketHandler.waiters.remove(self)
+
+    @classmethod
+    def update_cache(cls, chat):
+        cls.cache.append(chat)
+        if len(cls.cache) > cls.cache_size:
+            cls.cache = cls.cache[-cls.cache_size:]
+
+    @classmethod
+    def send_updates(cls, chat):
+        logging.info("sending message to %d waiters", len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                logging.error("Error sending message", exc_info=True)
+
+    def on_message(self, message):
+        logging.info("got message %r", message)
+        parsed = tornado.escape.json_decode(message)
+        chat = {
+            "id": str(uuid.uuid4()),
+            "body": parsed["body"],
+            }
+        chat["html"] = tornado.escape.to_basestring(
+            self.render_string("message.html", message=chat))
+
+        ChatSocketHandler.update_cache(chat)
+        ChatSocketHandler.send_updates(chat)
 
 
+def main():
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
 
-if __name__ == '__main__':
-    import logging
-    logging.getLogger().setLevel(logging.DEBUG)
-    ChatRouter = sockjs.tornado.SockJSRouter(chatServer, '/chat')
-    application = tornado.web.Application(
-        [(r"/", MainHandler)] + ChatRouter.urls
-        )
-    application.listen(8765)
-    io_loop = tornado.ioloop.IOLoop.instance()
-    try:
-        io_loop.start()
-    except SystemExit, KeyboardInterrupt:
-        io_loop.stop()
+
+if __name__ == "__main__":
+    main()
